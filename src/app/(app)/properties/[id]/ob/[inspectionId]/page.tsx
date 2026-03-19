f78e0ea6e632761603c4221e70f1a04617ed7f21
Ditt Namn <din@mail.se>
2026-03-19 14:12:33 +0100

    ram

--- src/app/(app)/properties/[id]/ob/[inspectionId]/page.tsx
+++ src/app/(app)/properties/[id]/ob/[inspectionId]/page.tsx
@@ -409,7 +409,13 @@ export default function InspectionDetailPage() {
               </nav>
             </div>
 
-            <div className="rounded-2xl border border-white/45 bg-white/95 p-3 shadow-xl ring-1 ring-black/5 md:p-4">
+            <div
+              className={
+                activeSection === 'insida'
+                  ? 'p-0 md:p-0'
+                  : 'rounded-2xl border border-white/45 bg-white/95 p-3 shadow-xl ring-1 ring-black/5 md:p-4'
+              }
+            >
               <ObWizard
                 property={property}
                 inspection={inspection}
